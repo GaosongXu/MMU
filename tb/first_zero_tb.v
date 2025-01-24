80c0efc5c2982bce38eadddf3d5696fe44a31072
GaosongXu <shimaring@qq.com>
2025-01-24 23:16:57 +0800

    update first_zero_tb module

--- tb/first_zero_tb.v
+++ tb/first_zero_tb.v
@@ -1,6 +1,6 @@
 
 //DUT: firstZero.v
-`include "C:/Users/11508/Desktop/LPIO_CODE/MMU/src/firstZero.v"
+`include "src/first_zero.v"
 
 module first_zero_tb;
 
@@ -21,7 +21,7 @@ module first_zero_tb;
     end
 
     // DUT instantiation
-    FirstZero FirstZero_inst (
+    first_zero first_zero_i (
         .clk(clk),
         .rst_n(rst_n),
         .data_in(data_in),
@@ -41,44 +41,41 @@ module first_zero_tb;
         rst_n = 1;
 
         // Test case 1: No zero in data_in
-        #10;
-        data_in = 64'hFFFFFFFFFFFFFFFF;
-        #10;
+        #20;
+        data_in = 64'hFFFFFFFFFFF0FFFF;
         $display("Test case 1: data_in = %h, find_success = %b, pos_out = %d, mask_out = %h", data_in, find_success, pos_out, mask_out);
 
         // Test case 2: Zero at position 0
-        #10;
+        #20;
         data_in = 64'hFFFFFFFFFFFFFFFE;
-        #10;
         $display("Test case 2: data_in = %h, find_success = %b, pos_out = %d, mask_out = %h", data_in, find_success, pos_out, mask_out);
 
         // Test case 3: Zero at position 63
-        #10;
+       #20;
         data_in = 64'h7FFFFFFFFFFFFFFF;
-        #10;
         $display("Test case 3: data_in = %h, find_success = %b, pos_out = %d, mask_out = %h", data_in, find_success, pos_out, mask_out);
 
         // Test case 4: Multiple zeros
-        #10;
+       #20;
         data_in = 64'hFF00FF00FF00FF00;
-        #10;
+        
         $display("Test case 4: data_in = %h, find_success = %b, pos_out = %d, mask_out = %h", data_in, find_success, pos_out, mask_out);
 
         // Test case 5: All zeros
-        #10;
+       #20;
         data_in = 64'h0000000000000000;
-        #10;
+       
         $display("Test case 5: data_in = %h, find_success = %b, pos_out = %d, mask_out = %h", data_in, find_success, pos_out, mask_out);
 
         // Test case 6: First zero at position 0
-        #10;
+       #20;
         data_in = 64'h7FFFFFFFFFFFFFFF;
-        #10;
+        
         $display("Test case 6: data_in = %h, find_success = %b, pos_out = %d, mask_out = %h", data_in, find_success, pos_out, mask_out);
 
 
         // Finish simulation
-        #10;
+        #20;
         $finish;
     end
 
